093ae577ef945a7027ed1e6404ece423b7d56bc6
liavivi9 <liaavivi9@gmail.com>
2025-02-21 18:37:59 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,147 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    mysprite2.setPosition(randint(0, 160), randint(0, 120))
+    fruit = randint(0, 4)
+    if (fruit == 0) {
+        mysprite2.setImage(img`
+            . . . . . . . . . . b b b . . . 
+            . . . . . . . . b e e 3 3 b . . 
+            . . . . . . b b e 3 2 e 3 a . . 
+            . . . . b b 3 3 e 2 2 e 3 3 a . 
+            . . b b 3 3 3 3 3 e e 3 3 3 a . 
+            b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+            b 3 3 3 d d d d 3 3 3 3 3 d d a 
+            b b b b b b b 3 d d d d d d 3 a 
+            b d 5 5 5 5 d b b b a a a a a a 
+            b 3 d d 5 5 5 5 5 5 5 d d d d a 
+            b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+            b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+            b b b 3 d 5 5 5 5 5 5 5 d d b a 
+            . . . b b b 3 d 5 5 5 5 d d 3 a 
+            . . . . . . b b b b 3 d d d b a 
+            . . . . . . . . . . b b b a a . 
+            `)
+    } else if (fruit) {
+        mysprite2.setImage(img`
+            . . . . . 3 3 b 3 3 d d 3 3 . . 
+            . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+            . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+            . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+            . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+            . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+            . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+            . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+            . 4 4 d 1 1 1 1 1 1 d d d b b . 
+            . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+            4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+            4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+            4 5 5 d 5 5 d b b b d d 3 . . . 
+            4 5 5 5 d d d d 4 4 b 3 . . . . 
+            . 4 5 5 5 4 4 4 . . . . . . . . 
+            . . 4 4 4 . . . . . . . . . . . 
+            `)
+    } else if (fruit) {
+        mysprite2.setImage(img`
+            . . . . . . b b b b . . . . . . 
+            . . . . . . b 4 4 4 b . . . . . 
+            . . . . . . b b 4 4 4 b . . . . 
+            . . . . . b 4 b b b 4 4 b . . . 
+            . . . . b d 5 5 5 4 b 4 4 b . . 
+            . . . . b 3 2 3 5 5 4 e 4 4 b . 
+            . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+            . . . b 5 3 2 3 5 5 5 5 e e e e 
+            . . b d 7 5 5 5 3 2 3 5 5 e e e 
+            . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+            . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+            . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+            b d 3 2 d 5 5 5 d d d 4 4 . . . 
+            b 5 5 5 5 d d 4 4 4 4 . . . . . 
+            4 d d d 4 4 4 . . . . . . . . . 
+            4 4 4 4 . . . . . . . . . . . . 
+            `)
+    } else {
+        mysprite2.setImage(img`
+            . . . . c c c b b b b b . . . . 
+            . . c c b 4 4 4 4 4 4 b b b . . 
+            . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+            . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+            e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+            e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+            e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+            . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+            8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+            8 7 2 e e e e e e e e e e 2 7 8 
+            e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+            e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+            e b e 8 8 c c 8 8 c c c 8 e b e 
+            e e b e c c e e e e e c e b e e 
+            . e e b b 4 4 4 4 4 4 4 4 e e . 
+            . . . c c c c c e e e e e . . . 
+            `)
+    }
+})
+let fruit = 0
+let mysprite2: Sprite = null
+scene.setBackgroundColor(13)
+let כרישי = sprites.create(img`
+    .................ccfff..............
+    ................cddbbf..............
+    ...............cddbbf...............
+    ..............fccbbcf............ccc
+    ........ffffffccccccff.........ccbbc
+    ......ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ....ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    ....fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    ....fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .....fb11111111bbbbbbcccccccccbccf..
+    ......fccc33cc11bbbbccccccccfffbbcf.
+    .......fc131c111bbbcccccbdbc...fbbf.
+    ........f33c111cbbbfdddddcc.....fbbf
+    .........ff1111fbdbbfddcc........fff
+    ...........cccccfbdbbfc.............
+    .................fffff..............
+    `, SpriteKind.Player)
+controller.moveSprite(כרישי, 100, 100)
+animation.runImageAnimation(
+כרישי,
+[img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `],
+200,
+true
+)
+mysprite2 = sprites.create(img`
+    . . . . . . . . . . b b b . . . 
+    . . . . . . . . b e e 3 3 b . . 
+    . . . . . . b b e 3 2 e 3 a . . 
+    . . . . b b 3 3 e 2 2 e 3 3 a . 
+    . . b b 3 3 3 3 3 e e 3 3 3 a . 
+    b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+    b 3 3 3 d d d d 3 3 3 3 3 d d a 
+    b b b b b b b 3 d d d d d d 3 a 
+    b d 5 5 5 5 d b b b a a a a a a 
+    b 3 d d 5 5 5 5 5 5 5 d d d d a 
+    b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+    b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+    b b b 3 d 5 5 5 5 5 5 5 d d b a 
+    . . . b b b 3 d 5 5 5 5 d d 3 a 
+    . . . . . . b b b b 3 d d d b a 
+    . . . . . . . . . . b b b a a . 
+    `, SpriteKind.Player)
+כרישי.setStayInScreen(true)
+mysprite2.setStayInScreen(true)
